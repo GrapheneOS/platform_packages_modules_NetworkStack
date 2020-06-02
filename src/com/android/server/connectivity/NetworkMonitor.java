c4b59dd64f375ad4c025c3018136658ecf21d27b
lucaslin <lucaslin@google.com>
2020-06-02 16:31:21 +0800

    Add a null check to prevent NullPointerException
    TelephonyManager#getAllCellInfo() may return null, so before
    running the foreach loop, the null check is needed.
    
    Bug: 156567547
    Test: atest NetworkStackTests:NetworkMonitorTest
    Change-Id: I5d5e98add0ea2abf92156c11188739c535e7e9a8

--- src/com/android/server/connectivity/NetworkMonitor.java
+++ src/com/android/server/connectivity/NetworkMonitor.java
@@ -1716,6 +1716,7 @@ public class NetworkMonitor extends StateMachine {
         }
         try {
             final List<CellInfo> cells = mTelephonyManager.getAllCellInfo();
+            if (cells == null) return null;
             final Map<String, Integer> countryCodeMap = new HashMap<>();
             int maxCount = 0;
             for (final CellInfo cell : cells) {
