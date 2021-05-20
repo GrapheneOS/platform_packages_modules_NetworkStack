eb5553205995dff4ffd6f1571d930acdadef0596
Pratyush <39559233+empratyush@users.noreply.github.com>
2021-05-20 13:21:31 +0530

    ipv4 leak fix

--- src/android/net/dhcp/DhcpClient.java
+++ src/android/net/dhcp/DhcpClient.java
@@ -74,6 +74,9 @@ import android.net.util.InterfaceParams;
 import android.net.util.NetworkStackUtils;
 import android.net.util.PacketReader;
 import android.net.util.SocketUtils;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
@@ -111,6 +114,8 @@ import java.net.SocketAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -1198,7 +1203,7 @@ public class DhcpClient extends StateMachine {
                                 + " lease expiry: "  + attributes.assignedV4AddressExpiry
                                 + " current time: "  + currentTime);
                     }
-                    if (currentTime >= attributes.assignedV4AddressExpiry) {
+                    if (currentTime >= attributes.assignedV4AddressExpiry || shouldAvoidUsingPreviousIpv4()) {
                         // Lease has expired.
                         transitionTo(mDhcpInitState);
                         return HANDLED;
@@ -1223,6 +1228,31 @@ public class DhcpClient extends StateMachine {
         }
     }
 
+    private static final int RANDOMIZATION_ALWAYS = 100;
+    
+    private boolean shouldAvoidUsingPreviousIpv4(){
+
+    try {
+        WifiManager wifiManager = (WifiManager)  mContext.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo =  wifiManager.getConnectionInfo();
+        if(wifiInfo != null){
+            int connectedNetworkId = wifiInfo.getNetworkId();
+            List<WifiConfiguration> configurationList = wifiManager.getConfiguredNetworks();
+            for(WifiConfiguration configuration : configurationList){
+                if(configuration.networkId ==  connectedNetworkId){
+                    return configuration.macRandomizationSetting == RANDOMIZATION_ALWAYS;
+                }
+            }
+        }
+
+    }catch (Exception e){
+        loge(e.getLocalizedMessage(), e);
+    }
+
+    loge("ConfiguredNetworks should contain Connected network id config ");
+    return true;
+}
+    
     private void receiveOfferOrAckPacket(final DhcpPacket packet, final boolean acceptRapidCommit) {
         if (!isValidPacket(packet)) return;
 
