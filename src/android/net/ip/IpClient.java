926c6d7b452b9ef8610324743382fa35e5488441
Xiao Ma <xiaom@google.com>
2023-08-09 16:50:12 +0900

    Add null check for mPrefixDelegation in clearIpv6PrefixDelegationAddresses.
    Ideally mPrefixDelegation should not be null when IpClient receives the
    DHCPv6-PD failure messages (e.g. prefix expires or renewal prefix does
    not match the request one), these failure should only happen after we
    get a success PD. Add a null check in this function and Log.wtf to see
    if we can hit this error in practice.
    
    Bug: 290885706
    Test: atest NetworkStackIntegrationTests
    Change-Id: Ibfd2240d4cc3120c8e5de0cdf69a786a9b009118

--- src/android/net/ip/IpClient.java
+++ src/android/net/ip/IpClient.java
@@ -2802,6 +2802,10 @@ public class IpClient extends StateMachine {
         }
 
         private void clearIpv6PrefixDelegationAddresses() {
+            if (mPrefixDelegation == null) {
+                Log.wtf(mTag, "PrefixDelegation shouldn't be null when DHCPv6 PD fails.");
+                return;
+            }
             final IpPrefix prefix;
             try {
                 prefix = new IpPrefix(Inet6Address.getByAddress(mPrefixDelegation.ipo.prefix),
