491d918245acf4d448b8758c74e7b3424ce20f07
Chiachang Wang <chiachangwang@google.com>
2023-09-25 02:50:58 +0000

    Use the ratio of total TCP retransmits as detection signal
    The original fail rate is calculated from tcpi_lost and
    tcpi_retransmits. The calculation uses the difference between
    2 snapshots of the TCP socket statistic. The tcpi_retransmits
    field is the number of recurring retransmission timeout of
    first sequenced that is not acknowledged yet and will increase
    once per retrasmit of a single packet. The tcpi_lost is the
    current lost packets beyond the acked sequence and will increase
    by one for packets that are not marked as TCPCB_LOST and
    TCPCB_SACKED_ACKED. These two numbers are not of the same
    dimension. This will cause the fail rate result over 100%.
    
    In this case, tcpi_total_retrans is a better match as it's the
    cumulative retransmitted packets. The retransmitted packets
    should be a good enough signal because the network should be
    in a bad shape if device keep sending retransmission without
    acks. Thus, replace the indicator to use tcpi_total_retrans as
    the nominator of the fail rate, and remove the unused lost
    and retransmits fields in TcpInfo to reduce structure size.
    
    Bug: 265107451
    Test: atest NetworkStacktTests
    Change-Id: Ib3ea3681fa5cbfaaef653ea53d12a7c9257db124

--- src/com/android/networkstack/netlink/TcpInfo.java
+++ src/com/android/networkstack/netlink/TcpInfo.java
@@ -98,10 +98,32 @@ public class TcpInfo {
     static final int SEGS_OUT_OFFSET = getFieldOffset(Field.SEGS_OUT);
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     static final int TOTAL_RETRANS_OFFSET = getFieldOffset(Field.TOTAL_RETRANS);
+    /**
+     * This counts individual incoming packets that appeared on the wire, including:
+     * SYN, SYN-ACK, pure ACKs, data segments (after segmentation offload into small <=mtu
+     * packets), FIN, FIN-ACK, and any retransmits.
+     *
+     * This field is read from the tcpi_segs_in field from {@code struct tcp_info}
+     * in bionic/libc/kernel/uapi/linux/tcp.h. Also see [tcpEStatsPerfSegsIn] in the RFC4898.
+     */
     final int mSegsIn;
+    /**
+     * This counts individual outgoing packets that have been sent to the network, including:
+     * SYN, SYN-ACK, pure ACKs, data segments (after segmentation offload into small <=mtu
+     * packets), FIN, FIN-ACK, and any retransmits.
+     *
+     * This field is read from the tcpi_segs_out field from {@code struct tcp_info}
+     * in bionic/libc/kernel/uapi/linux/tcp.h. Also see [tcpEStatsPerfSegsOut] in the RFC4898.
+     */
     final int mSegsOut;
-    final int mLost;
-    final int mRetransmits;
+    /**
+     * This counts individual accumulated retransmitted packets that have been sent to the network,
+     * including any retransmits for SYN, SYN-ACK, pure ACKs, data segments (after segmentation
+     * offload into small <=mtu packets), FIN and FIN-ACK.
+     *
+     * This field is read from the tcpi_total_retrans field from {@code struct tcp_info}
+     * in bionic/libc/kernel/uapi/linux/tcp.h.
+     */
     final int mTotalRetrans;
 
     private static int getFieldOffset(@NonNull final Field needle) {
@@ -122,8 +144,6 @@ public class TcpInfo {
         final int start = bytes.position();
         mSegsIn = bytes.getInt(start + SEGS_IN_OFFSET);
         mSegsOut = bytes.getInt(start + SEGS_OUT_OFFSET);
-        mLost = bytes.getInt(start + LOST_OFFSET);
-        mRetransmits = bytes.get(start + RETRANSMITS_OFFSET);
         mTotalRetrans = bytes.get(start + TOTAL_RETRANS_OFFSET);
         // tcp_info structure grows over time as new fields are added. Jump to the end of the
         // structure, as unknown fields might remain at the end of the structure if the tcp_info
@@ -132,9 +152,7 @@ public class TcpInfo {
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    TcpInfo(int retransmits, int lost, int segsOut, int segsIn, int totalRetrans) {
-        mRetransmits = retransmits;
-        mLost = lost;
+    TcpInfo(int segsOut, int segsIn, int totalRetrans) {
         mSegsOut = segsOut;
         mSegsIn = segsIn;
         mTotalRetrans = totalRetrans;
@@ -184,18 +202,17 @@ public class TcpInfo {
         TcpInfo other = (TcpInfo) obj;
 
         return mSegsIn == other.mSegsIn && mSegsOut == other.mSegsOut
-                && mRetransmits == other.mRetransmits && mLost == other.mLost
                 && mTotalRetrans == other.mTotalRetrans;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(mLost, mRetransmits, mSegsIn, mSegsOut, mTotalRetrans);
+        return Objects.hash(mSegsIn, mSegsOut, mTotalRetrans);
     }
 
     @Override
     public String toString() {
-        return "TcpInfo{lost=" + mLost + ", retransmit=" + mRetransmits + ", received=" + mSegsIn
-                + ", sent=" + mSegsOut + ", totalRetrans=" + mTotalRetrans + "}";
+        return "TcpInfo{received=" + mSegsIn + ", sent=" + mSegsOut
+                + ", totalRetrans=" + mTotalRetrans + "}";
     }
 }
