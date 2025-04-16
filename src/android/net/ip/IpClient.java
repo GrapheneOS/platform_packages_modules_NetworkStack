c0f8aa656ddf0a92c9c80c1b6fc20c9314d29218
Xiao Ma <xiaom@google.com>
2025-04-16 13:04:21 +0900

    DHCPv6 P flag: remove the experiment flag from IpClient.
    Flag: EXEMPT guarded by an legacy experiment flag
    Bug: 315069348
    Test: TH
    Change-Id: I7335d3b42b47fac4ffeaedbceb59ed37fe68f576

--- src/android/net/ip/IpClient.java
+++ src/android/net/ip/IpClient.java
@@ -832,7 +832,6 @@ public class IpClient extends StateMachine {
     private final boolean mApfHandleIpv4PingOffload;
     private final boolean mApfHandleIpv6PingOffload;
     private final boolean mIgnoreNudFailureEnabled;
-    private final boolean mDhcp6PdPreferredFlagEnabled;
     private final boolean mReplaceNetdWithNetlinkEnabled;
 
     private InterfaceParams mInterfaceParams;
@@ -1138,12 +1137,13 @@ public class IpClient extends StateMachine {
         mNudFailureCountWeeklyThreshold = mDependencies.getDeviceConfigPropertyInt(
                 CONFIG_NUD_FAILURE_COUNT_WEEKLY_THRESHOLD,
                 DEFAULT_NUD_FAILURE_COUNT_WEEKLY_THRESHOLD);
-        mDhcp6PdPreferredFlagEnabled =
-                mDependencies.isFeatureEnabled(mContext, IPCLIENT_DHCPV6_PD_PREFERRED_FLAG_VERSION);
         mReplaceNetdWithNetlinkEnabled = mDependencies.isFeatureEnabled(mContext,
                 IPCLIENT_REPLACE_NETD_WITH_NETLINK_VERSION);
         IpClientLinkObserver.Configuration config = new IpClientLinkObserver.Configuration(
-                mAcceptRaMinLft, mPopulateLinkAddressLifetime, mDhcp6PdPreferredFlagEnabled);
+                mAcceptRaMinLft,
+                mPopulateLinkAddressLifetime,
+                mDependencies.isFeatureEnabled(mContext,
+                        IPCLIENT_DHCPV6_PD_PREFERRED_FLAG_VERSION));
 
         mLinkObserver = new IpClientLinkObserver(
                 mContext, getHandler(),
