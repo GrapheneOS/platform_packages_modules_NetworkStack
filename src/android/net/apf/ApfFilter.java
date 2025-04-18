ee6153e8a4d54c769affe4b4a27d0d8e11f6472f
Yuyang Huang <yuyanghuang@google.com>
2025-04-18 19:40:32 +0900

    Enable APF counter metrics on V+ devices with APFv4+
    This commit enables the collection of APF counter metrics for all V and
    later devices that support APFv4 and above. The introduction of
    ApfIntegrationTest in Android V provides some assurance that the APF
    counter region can be collected correctly. With the significant number
    of APFv6 features introduced in Android B, collecting counter metrics
    from non-Pixel devices will be valuable. The polling interval is set to
    30 minutes to minimize potential battery impact. A follow-up CL will
    improve polling accuracy and reduce bias by transitioning from a
    TimerFD-based solution.
    
    Test: TH
    Flag: EXEMPT mainline
    Change-Id: I82a17a036ae31af81f30314041f7fa3f7c967ffa

--- src/android/net/apf/ApfFilter.java
+++ src/android/net/apf/ApfFilter.java
@@ -380,7 +380,6 @@ public class ApfFilter {
     private final InterfaceParams mInterfaceParams;
     private final TokenBucket mTokenBucket;
 
-    @VisibleForTesting
     public final int mApfVersionSupported;
     @VisibleForTesting
     @NonNull
