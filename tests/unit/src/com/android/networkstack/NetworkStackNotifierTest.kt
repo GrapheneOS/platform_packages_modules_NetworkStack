f8aa6dec4a440aa539ace175d163277369e09b7e
Yuyang Huang <yuyanghuang@google.com>
2025-11-04 17:08:47 +0900

    Use ktlint to format NetworkStackNotifierTest.kt
    Test: TH
    Flag: EXEMPT TEST_ONLY
    Change-Id: I84879d0335e8146bdc68b2b29b4c99d16ab3b4cd

--- tests/unit/src/com/android/networkstack/NetworkStackNotifierTest.kt
+++ tests/unit/src/com/android/networkstack/NetworkStackNotifierTest.kt
@@ -45,12 +45,15 @@ import android.testing.TestableLooper.RunWithLooper
 import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.android.dx.mockito.inline.extended.ExtendedMockito.verify
+import com.android.modules.utils.build.SdkLevel.isAtLeastS
 import com.android.networkstack.NetworkStackNotifier.CHANNEL_CONNECTED
 import com.android.networkstack.NetworkStackNotifier.CHANNEL_VENUE_INFO
 import com.android.networkstack.NetworkStackNotifier.CONNECTED_NOTIFICATION_TIMEOUT_MS
 import com.android.networkstack.NetworkStackNotifier.Dependencies
 import com.android.networkstack.apishim.NetworkInformationShimImpl
-import com.android.modules.utils.build.SdkLevel.isAtLeastS
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.Test
@@ -65,9 +68,6 @@ import org.mockito.Mockito.any
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.never
 import org.mockito.MockitoAnnotations
-import kotlin.reflect.KClass
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 @RunWith(AndroidTestingRunner::class)
 @SmallTest
@@ -124,8 +124,11 @@ class NetworkStackNotifierTest {
             val captivePortalDataShim = networkShim.getCaptivePortalData(this)
 
             if (captivePortalDataShim != null) {
-                networkShim.setCaptivePortalData(this, captivePortalDataShim
-                        .withVenueFriendlyName(TEST_NETWORK_FRIENDLY_NAME))
+                networkShim.setCaptivePortalData(
+                    this,
+                    captivePortalDataShim
+                        .withVenueFriendlyName(TEST_NETWORK_FRIENDLY_NAME)
+                )
             }
         }
     }
@@ -159,26 +162,41 @@ class NetworkStackNotifierTest {
         doReturn(realContext.packageName).`when`(mContext).packageName
 
         doReturn(mCurrentUserContext).`when`(mContext).createPackageContextAsUser(
-                realContext.packageName, 0, UserHandle.CURRENT)
+            realContext.packageName,
+            0,
+            UserHandle.CURRENT
+        )
         doReturn(mAllUserContext).`when`(mContext).createPackageContextAsUser(
-                realContext.packageName, 0, UserHandle.ALL)
+            realContext.packageName,
+            0,
+            UserHandle.ALL
+        )
 
         mAllUserContext.mockService(Context.NOTIFICATION_SERVICE, NotificationManager::class, mNm)
-        mContext.mockService(Context.NOTIFICATION_SERVICE, NotificationManager::class,
-                mNotificationChannelsNm)
+        mContext.mockService(
+            Context.NOTIFICATION_SERVICE,
+            NotificationManager::class,
+            mNotificationChannelsNm
+        )
         mContext.mockService(Context.CONNECTIVITY_SERVICE, ConnectivityManager::class, mCm)
 
         doReturn(NotificationChannel(CHANNEL_VENUE_INFO, "TestChannel", IMPORTANCE_DEFAULT))
                 .`when`(mNotificationChannelsNm).getNotificationChannel(CHANNEL_VENUE_INFO)
 
         doReturn(mPendingIntent).`when`(mDependencies).getActivityPendingIntent(
-                any(), any(), anyInt())
+            any(),
+            any(),
+            anyInt()
+        )
         mNotifier = NetworkStackNotifier(mContext, mLooper.looper, mDependencies)
         mHandler = mNotifier.handler
 
         val allNetworksCbCaptor = ArgumentCaptor.forClass(NetworkCallback::class.java)
-        verify(mCm).registerNetworkCallback(any() /* request */, allNetworksCbCaptor.capture(),
-                eq(mHandler))
+        verify(mCm).registerNetworkCallback(
+            any(),
+            allNetworksCbCaptor.capture(),
+            eq(mHandler)
+        )
         mAllNetworksCb = allNetworksCbCaptor.value
 
         val defaultNetworkCbCaptor = ArgumentCaptor.forClass(NetworkCallback::class.java)
@@ -212,8 +230,10 @@ class NetworkStackNotifierTest {
             "Connected notifications should be local only"
         )
         verify(mDependencies).getActivityPendingIntent(
-                eq(mCurrentUserContext), mIntentCaptor.capture(),
-                intThat { it or FLAG_IMMUTABLE != 0 })
+            eq(mCurrentUserContext),
+            mIntentCaptor.capture(),
+            intThat { it or FLAG_IMMUTABLE != 0 }
+        )
     }
 
     private fun verifyCanceledNotificationAfterNetworkLost() {
@@ -296,8 +316,10 @@ class NetworkStackNotifierTest {
 
         verify(mNm).notify(eq(TEST_NETWORK_TAG), mNoteIdCaptor.capture(), mNoteCaptor.capture())
         verify(mDependencies).getActivityPendingIntent(
-                eq(mCurrentUserContext), mIntentCaptor.capture(),
-                intThat { it or FLAG_IMMUTABLE != 0 })
+            eq(mCurrentUserContext),
+            mIntentCaptor.capture(),
+            intThat { it or FLAG_IMMUTABLE != 0 }
+        )
         verifyVenueInfoIntent(mIntentCaptor.value)
         assertTrue(
             mNoteCaptor.value.flags and Notification.FLAG_LOCAL_ONLY != 0,
@@ -361,8 +383,11 @@ class NetworkStackNotifierTest {
         verify(mResources).getString(R.string.tap_for_info)
         verify(mNm).notify(eq(TEST_NETWORK_TAG), mNoteIdCaptor.capture(), mNoteCaptor.capture())
         val note = mNoteCaptor.value
-        assertEquals(TEST_NETWORK_FRIENDLY_NAME, note.extras
-                .getCharSequence(Notification.EXTRA_TITLE))
+        assertEquals(
+            TEST_NETWORK_FRIENDLY_NAME,
+            note.extras
+                .getCharSequence(Notification.EXTRA_TITLE)
+        )
         verifyCanceledNotificationAfterDefaultNetworkLost()
     }
 
